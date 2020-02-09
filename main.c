cca1c75a24aa023a8b38f15457ac20f005267f81
Brian Button <bbutton@agilestl.com>
2020-02-09 08:39:49 -0800

    Added ability to start without specifying a directory

--- main.c
+++ main.c
@@ -7,28 +7,12 @@ int main(int argc, char ** argv) {
   queue queue;
   initialize(&queue);
 
-  printf("Beginnning traversal at %s\n", argv[1]);
-  
-  traverse_directory_tree(&queue, argv[1], argv[0]);
-
-  printf("ended traversal\n");
+  if(argc == 1) {
+	traverse_directory_tree(&queue, ".", argv[0]);
+  }
+  else {
+	traverse_directory_tree(&queue, argv[1], argv[0]);
+  }	
 
   return 0;
 }
-
-
-
-/*
- * breadthfirst ( root )
-{
-    enqueue ( root );
-    while ( ! empty ( queue ) )
-    {
-next = dequeue ( queue );
-for each node directly below next {
- } }
-visit ( node );
-if ( isa_directory ( node ) )
-    enqueue ( node );
-}
- */
